a765681a1f02403bd9d91df855185e6adfc269c5
rafael <dragonwood0@protonmail.com>
2021-02-23 23:43:52 +0800

    Generalized the input fields into one component - Section.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,5 @@
 import React, {Component} from "react";
-import GeneralInfo from "./GeneralInfo";
-import Experience from "./Experience";
-import Education from "./Education";
+import Section from "./Section";
 
 class Resume extends Component {
   constructor() {
@@ -27,21 +25,21 @@ class Resume extends Component {
         jobTitle: "",
         dateStart: "",
         dateEnd: "",
-        mainJobTasks: "", // TODO: make this an array of tasks
+        jobSummary: "", // TODO: make this an array of tasks
                           // then display in bullets
       },
     };
   }
 
-  handleChange = (e, componentName) => {
-    const changedInfo = e.target.name;
-    const component = componentName;
+  handleChange = (e, sectionName) => {
+    const modifiedField = e.target.name;
+    const section = sectionName;
 
     this.setState({
-      [component]: {
-        ...this.state[component], 
+      [section]: {
+        ...this.state[section], 
         // sets whatever input field is changed
-        [changedInfo]: e.target.value
+        [modifiedField]: e.target.value
       }
     })
   }
@@ -51,6 +49,45 @@ class Resume extends Component {
 
     return (
       <form onSubmit={this.handleSubmit}>
+        <Section
+          fields={[
+            ["name", "text", genInfo.name],
+            ["position", "text", genInfo.position],
+            ["phoneNum", "number", genInfo.phoneNum],
+            ["email", "email", genInfo.email]
+          ]}
+          handleChange={this.handleChange}
+          section="genInfo"
+        />
+        <Section
+          fields={[
+            ["schoolName", "text", education.schoolName],
+            ["schoolLocation", "text", education.schoolLocation],
+            ["degree", "text", education.degree],
+            ["dateStart", "text", education.dateStart],
+            ["dateEnd", "text", education.dateEnd]
+          ]}
+          handleChange={this.handleChange}
+          section="education"
+        />
+        <Section
+          fields={[
+            ["company", "text", experience.company],
+            ["location", "text", experience.location],
+            ["jobTitle", "text", experience.jobTitle],
+            ["jobSummary", "text", experience.jobSummary],
+            ["dateStart", "text", experience.dateStart],
+            ["dateEnd", "text", experience.dateEnd]
+          ]}
+          handleChange={this.handleChange}
+          section="experience"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+/*
         <GeneralInfo 
           genInfo={genInfo} 
           handleChange={this.handleChange}
@@ -66,10 +103,6 @@ class Resume extends Component {
           handleChange={this.handleChange}
           component="education"
         />
-        <input type="submit" value="Submit" />
-      </form>
-    )
-  }
-}
+        */
 
 export default Resume
---
+++ src/components/Section.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Section extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { fields, handleChange, section } = this.props;
+    console.log(fields)
+    console.log(section);
+
+    const onchange = e => handleChange(e, section);
+
+    const inputElements = fields.map((field, idx) => {
+      return( 
+        <input 
+          key={idx}
+          name={field[0]} 
+          type={field[1]} 
+          value={field[2]}
+          onChange={onchange}
+        />
+      )
+    })
+
+    return (
+      <div className="input-group">
+        {inputElements}
+      </div>
+    )
+  }
+}
+
+export default Section
